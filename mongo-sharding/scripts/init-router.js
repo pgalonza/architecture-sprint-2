80d89498f8d15581962270000aeefbeaf984188a
Peter Galonza <p.galonza@evaron.ru>
2024-11-03 02:38:32 +0300

    feature: add task2

---
+++ mongo-sharding/scripts/init-router.js
@@ -0,0 +1,2 @@
+sh.addShard("rs-shard-01/shard-01-node-a:27017")
+sh.addShard("rs-shard-02/shard-02-node-a:27017")
---
+++ mongo-sharding/scripts/init-shard02.js
@@ -0,0 +1,1 @@
+rs.initiate({_id: "rs-shard-02", version: 1, members: [ { _id: 0, host : "shard-02-node-a:27017" }, ] })
